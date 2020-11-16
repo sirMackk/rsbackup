27d1a8b9a892c0c1550c86034293c36a377ef30b
sirMackk <matt@mattscodecave.com>
2020-11-15 17:03:22 -0800

    Add remaining functions/tests to pyclient

--- pyclient/client.py
+++ pyclient/client.py
@@ -1,17 +1,17 @@
 import asyncio
 import functools
+import hashlib
 import pathlib
+import typing
 
 import aiohttp
 import click
 
 # TODO:
-# - client: submit data, retrieve data, check data health
-# - cli util
-#   - custom ssl cert
-#   - server_url
+# - timeouts
+# - cli plumbing
+# - security: ssl, auth?
 # - typehints
-# - security: ssl for transport sec, authn
 # - docstrings
 
 
@@ -23,29 +23,106 @@ class ServerError(BackuperError):
     pass
 
 
+class ClientError(BackuperError):
+    pass
+
+
 class Client():
     SERVER_URLMAP = {
         'list_data': 'list_data',
+        'check_data': 'check_data',
+        'submit_data': 'submit_data',
+        'retrieve_data': 'retrieve_data',
     }
 
     def __init__(self, server_url: str = 'localhost:44987') -> None:
         self.server_url = server_url
 
-    async def submit_data(self, filepath: pathlib.Path) -> None:
-        with aiohttp.ClientSession() as session:
-            pass
+    def _sha256(self, file_: typing.BinaryIO) -> str:
+        file_.seek(0)
+        block_size = 65536
+        hasher = hashlib.sha256()
+        buf = file_.read(block_size)
+        while len(buf) > 0:
+            hasher.update(buf)
+            buf = file_.read(block_size)
+        file_.seek(0)
+        return hasher.hexdigest()
 
-    async def retrieve_data(self, uuid: str,
+    async def submit_data(self, fname: str, filepath: pathlib.Path) -> None:
+        # rsp = {submitted: {sha256}}
+        if not filepath.exists():
+            raise ClientError(f"{filepath} does not exist!")
+        if not filepath.is_file():
+            raise ClientError(f"{filepath} is not a file!")
+
+        async with aiohttp.ClientSession() as session:
+            with open(filepath, 'rb') as f:
+                sha256_digest = self._sha256(f)
+                print('=' * 80)
+                print(f'Uploading file: {filepath}')
+                print(f'sha256: {sha256_digest}')
+                async with session.post(
+                    f'{self.server_url}/{self.SERVER_URLMAP["submit_data"]}',
+                    data=f
+                ) as rsp:
+                    if rsp.status != 200:
+                        raise ServerError(await rsp.text())
+                    data_submit = (await rsp.json())['submitted']
+                    print('=' * 80)
+                    # TODO what if digests dont match?
+                    print('Status: SUCCESS')
+                    print(f'sha256: {data_submit["sha256"]}')
+                    print(
+                        f'sha256 match: {sha256_digest == data_submit["sha256"]}'
+                    )
+
+    async def _save_rsp_to_file(self, rsp: aiohttp.ClientResponse,
+                                path: pathlib.Path) -> None:
+        chunk_size = 66560
+        with open(path, 'wb') as f:
+            while True:
+                chunk = await rsp.content.read(chunk_size)
+                if not chunk:
+                    break
+                f.write(chunk)
+
+    async def retrieve_data(self, fname: str,
                             target_path: pathlib.Path) -> None:
-        with aiohttp.ClientSesion() as session:
-            pass
+        if target_path.exists():
+            raise ClientError(f'{target_path} already exists!')
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                f'{self.server_url}/{self.SERVER_URLMAP["retrieve_data"]}/{fname}'
+            ) as rsp:
+                if rsp.status == 400:
+                    raise ClientError(f'File {fname} not found!')
+                elif rsp.status != 200:
+                    raise ServerError(await rsp.text())
+                else:
+                    await self._save_rsp_to_file(rsp, target_path)
+                    print(f'Downloaded "{fname}" to "{target_path}"')
 
-    async def check_data(self, uuid: str) -> None:
-        with aiohttp.ClientSession() as session:
-            pass
+    async def check_data(self, fname: str) -> None:
+        # rsp = {status: {sha256, name, lmod, health}}
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                f'{self.server_url}/{self.SERVER_URLMAP["check_data"]}'
+            ) as rsp:
+                if rsp.status == 400:
+                    raise ClientError(f'File {fname} not found!')
+                elif rsp.status != 200:
+                    raise ServerError(await rsp.text())
+                else:
+                    data_check = (await rsp.json())['status']
+                    print('=' * 80)
+                    print(f'name: {data_check["name"]}')
+                    print(f'last modified: {data_check["lmod"]}')
+                    print(f'health: {data_check["health"]}')
+                    print(f'sha256: {data_check["sha256"]}')
 
     async def list_data(self) -> None:
-        # rsp = {files: [{uuid, sha256, name, lmod},]}
+        # rsp = {files: [{sha256, name, lmod},]}
         async with aiohttp.ClientSession() as session:
             async with session.get(
                     f'{self.server_url}/{self.SERVER_URLMAP["list_data"]}'
@@ -59,7 +136,6 @@ class Client():
                     print('=' * 80)
                     print(f'name: {file_["name"]}')
                     print(f'last modified: {file_["lmod"]}')
-                    print(f'uuid: {file_["uuid"]}')
                     print(f'sha256sum: {file_["sha256"]}')
 
 
--- pyclient/test_client.py
+++ pyclient/test_client.py
@@ -1,3 +1,5 @@
+import hashlib
+
 from pyclient import client as pyclient
 
 import pytest
@@ -6,6 +8,9 @@ from aioresponses import aioresponses
 
 SERVER_URL = 'example.com:8080'
 LIST_DATA_URL = f'{SERVER_URL}/{pyclient.Client.SERVER_URLMAP["list_data"]}'
+CHECK_DATA_URL = f'{SERVER_URL}/{pyclient.Client.SERVER_URLMAP["check_data"]}'
+SUBMIT_DATA_URL = f'{SERVER_URL}/{pyclient.Client.SERVER_URLMAP["submit_data"]}'
+RETRIEVE_DATA_URL = f'{SERVER_URL}/{pyclient.Client.SERVER_URLMAP["retrieve_data"]}'
 
 list_data_parameters = [
     ({
@@ -21,7 +26,6 @@ list_data_parameters = [
      '=' * 80 + '\n'
      'name: some/file/name\n'
      'last modified: 2020-11-14 15:49:34\n'
-     'uuid: 123\n'
      'sha256sum: 456\n'),
     ({'files': []}, 'No files!\n'),
 ]
@@ -29,7 +33,7 @@ list_data_parameters = [
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize('payload,expected', list_data_parameters)
-async def test_client_list_data(capfd, payload, expected) -> None:
+async def test_list_data(capfd, payload, expected) -> None:
     with aioresponses() as m:
         m.get(LIST_DATA_URL, status=200, payload=payload)
         c = pyclient.Client(SERVER_URL)
@@ -39,7 +43,7 @@ async def test_client_list_data(capfd, payload, expected) -> None:
 
 
 @pytest.mark.asyncio
-async def test_client_list_data_bad_reply(capfd) -> None:
+async def test_list_data_bad_reply() -> None:
     server_error_msg = 'Internal Server Error'
     with aioresponses() as m:
         m.get(LIST_DATA_URL, status=500, body=server_error_msg)
@@ -47,3 +51,146 @@ async def test_client_list_data_bad_reply(capfd) -> None:
         with pytest.raises(pyclient.ServerError) as e:
             await c.list_data()
         assert e.value.args[0] == server_error_msg
+
+
+@pytest.mark.asyncio
+async def test_check_data(capfd) -> None:
+    data_name = 'some/file'
+    payload = {
+        'status': {
+            'sha256': '123',
+            'name': 'some/file',
+            'lmod': '2020-11-15 15:49:34',
+            'health': 'GOOD'
+        }
+    }
+    expected = (
+        '=' * 80 + '\n'
+        'name: some/file\n'
+        'last modified: 2020-11-15 15:49:34\n'
+        'health: GOOD\n'
+        'sha256: 123\n'
+    )
+    with aioresponses() as m:
+        m.get(CHECK_DATA_URL, status=200, payload=payload)
+        c = pyclient.Client(SERVER_URL)
+        await c.check_data(data_name)
+        captured = capfd.readouterr()
+        assert captured.out == expected
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize('status,exc,exc_msg', [
+    (400, pyclient.ClientError, 'File some/data not found!'),
+    (500, pyclient.ServerError, 'Internal Server Error'),
+])
+async def test_check_data_bad_reply(status, exc, exc_msg) -> None:
+    with aioresponses() as m:
+        m.get(CHECK_DATA_URL, status=status, body=exc_msg)
+        c = pyclient.Client(SERVER_URL)
+        with pytest.raises(exc) as e:
+            await c.check_data('some/data')
+        assert e.value.args[0] == exc_msg
+
+
+@pytest.mark.asyncio
+async def test_submit_data(capfd, tmp_path) -> None:
+    status = 200
+    data_name = 'some/file'
+    data_path = tmp_path / 'filetosubmit'
+    data = b'1234' * 10
+    with open(data_path, 'wb') as f:
+        f.write(data)
+    sha256_digest = hashlib.sha256(data).hexdigest()
+    expected = (
+        '=' * 80 + '\n' +
+        f'Uploading file: {data_path}\n' +
+        f'sha256: {sha256_digest}\n' +
+        '=' * 80 + '\n' +
+        'Status: SUCCESS\n' +
+        f'sha256: {sha256_digest}\n' +
+        'sha256 match: True\n'
+    )
+    payload = {'submitted': {'sha256': sha256_digest}}
+
+    with aioresponses() as m:
+        m.post(SUBMIT_DATA_URL, status=status, payload=payload)
+        c = pyclient.Client(SERVER_URL)
+        await c.submit_data(data_name, data_path)
+        captured = capfd.readouterr()
+        assert captured.out == expected
+
+
+@pytest.mark.asyncio
+async def test_submit_data_no_file(capfd, tmp_path) -> None:
+    data_path = tmp_path / 'not-a-file'
+    expected = f'{data_path} does not exist!'
+
+    c = pyclient.Client(SERVER_URL)
+    with pytest.raises(pyclient.ClientError) as e:
+        await c.submit_data('some/file', data_path)
+    assert e.value.args[0] == expected
+
+
+@pytest.mark.asyncio
+async def test_submit_data_not_a_file(capfd, tmp_path) -> None:
+    data_path = tmp_path
+    expected = f'{data_path} is not a file!'
+
+    c = pyclient.Client(SERVER_URL)
+    with pytest.raises(pyclient.ClientError) as e:
+        await c.submit_data('some/file', data_path)
+    assert e.value.args[0] == expected
+
+
+@pytest.mark.asyncio
+async def test_submit_data_fails(capfd, tmp_path) -> None:
+    data_path = tmp_path / 'some-file'
+    data_path.touch()
+    expected = 'Internal Server Error'
+    with aioresponses() as m:
+        m.post(SUBMIT_DATA_URL, status=500, body=expected)
+        c = pyclient.Client(SERVER_URL)
+        with pytest.raises(pyclient.ServerError) as e:
+            await c.submit_data('some/file', data_path)
+        assert e.value.args[0] == expected
+
+
+@pytest.mark.asyncio
+async def test_retrieve_data(capfd, tmp_path) -> None:
+    data_path = tmp_path / 'target_file'
+    with aioresponses() as m:
+        m.get(RETRIEVE_DATA_URL + '/some/file', status=200, body=b'1234')
+        c = pyclient.Client(SERVER_URL)
+        await c.retrieve_data('some/file', data_path)
+
+        with open(data_path, 'rb') as f:
+            assert f.read() == b'1234'
+        captured = capfd.readouterr()
+        assert captured.out == f'Downloaded "some/file" to "{data_path}"\n'
+
+
+@pytest.mark.asyncio
+async def test_retrieve_data_already_exists(capfd, tmp_path) -> None:
+    data_path = tmp_path
+    c = pyclient.Client(SERVER_URL)
+    with pytest.raises(pyclient.ClientError) as e:
+        await c.retrieve_data('some/file', data_path)
+    assert e.value.args[0] == f'{data_path} already exists!'
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize('status,exc,exc_msg', [
+    (400, pyclient.ClientError, 'File some/file not found!'),
+    (500, pyclient.ServerError, 'Internal Server Error'),
+])
+async def test_retrieve_data_failure(capfd, tmp_path, status, exc,
+                                     exc_msg) -> None:
+    data_path = tmp_path / 'some/new/file'
+    with aioresponses() as m:
+        m.get(RETRIEVE_DATA_URL + '/some/file', status=status, body=exc_msg)
+        c = pyclient.Client(SERVER_URL)
+
+        with pytest.raises(exc) as e:
+            await c.retrieve_data('some/file', data_path)
+        assert e.value.args[0] == exc_msg
